d70f5adbc9109257703b5268abce0cb32ee98300
Keisuke Tottori <totto8492@gmail.com>
2022-09-03 00:21:25 +0900

    スクリプトをシーンから分離

---
+++ main.gd
@@ -0,0 +1,29 @@
+extends Control
+
+const URL := preload("res://url.gd")
+const HTTP := preload("res://http.gd")
+const DebugUtils := preload("res://debugutils.gd")
+
+var http := HTTP.new()
+
+
+func _ready() -> void:
+	const SAMPLE_URL := "https://httpbin.org/get"
+	var url := URL.new()
+	var err := url.parse(SAMPLE_URL)
+	assert(err == OK)
+	assert(url.to_string() == SAMPLE_URL)
+
+	var body: PackedByteArray = await http.request(url)
+	%Body.text = body.get_string_from_utf8()
+
+
+func _process(_delta: float) -> void:
+	var debug := DebugUtils.new()
+	var busy := http.is_busy()
+	var error := http.last_error
+	var status := debug.get_enum_key("HTTPClient", "Status", http.get_status())
+	var text := "busy: %s\nerror: %s\nstatus: %s" % [busy, error, status]
+
+	%Status.text = text
+	http.poll()
