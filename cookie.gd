2fe531c65864eec931188bd689f6c1ff894415d2
Keisuke Tottori <totto8492@gmail.com>
2022-09-05 15:38:33 +0900

    日時変換を追加

--- cookie.gd
+++ cookie.gd
@@ -11,6 +11,10 @@ func is_empty() -> bool:
 	return key.is_empty()
 
 
+func _to_string() -> String:
+	return str([key, value, expires, domain, path])
+
+
 static func make_from_header(header: String) -> Cookie:
 	var cookie := new()
 	var header_name := header.get_slice(":", 0).to_lower()
@@ -41,3 +45,23 @@ static func make_string_from_cookies(cookies: Array) -> String:
 		kvs.push_back("=".join([i.key, i.value]))
 
 	return "cookie: " + "; ".join(kvs)
+
+
+static func make_iso8601_from_rfc7231(from: String) -> String:
+	var regex_string := "^(?<week>\\w{3}), (?<day>\\d{2}) (?<month>\\w{3}) (?<year>\\d{4}) (?<hour>\\d{2}):(?<minute>\\d{2}):(?<second>\\d{2}) GMT$"
+	var regex := RegEx.create_from_string(regex_string)
+	var result := regex.search(from)
+	if result == null:
+		return ""
+
+	var dict := {}
+	for i in result.names:
+		dict[i] = result.strings[result.names[i]]
+
+	var monthly := ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+	var month_idx := monthly.find(dict["month"]) + 1
+	if month_idx == -1:
+		return ""
+
+	dict["month"] = str(month_idx)
+	return Time.get_datetime_string_from_datetime_dict(dict, false)
